3fcb8aef7e2a9379b5bb6e71c6c2a80ab00ec594
collinsc2065 <camicollins74@gmail.com>
2016-09-08 09:51:48 -0700

    Added Lesson 2

---
+++ PYLesson_02/PYLesson_02.py
@@ -0,0 +1,10 @@
+print ("name=ProfessorHandsome")
+print ("Name=ProfessorHandsome")
+print (8+5)
+num1 = 7
+num2 = 8
+print (num1 * num2)
+l = 8
+w = 5
+recArea = l * w
+print ("The area of your rectangle is",recArea)
