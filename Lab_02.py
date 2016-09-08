b367696198d1eff9b2849237b5f0c202e6a4486f
collinsc2065 <camicollins74@gmail.com>
2016-09-08 09:37:32 -0700

    Add files via upload

---
+++ Lab_02.py
@@ -0,0 +1,12 @@
+num1 = 8
+num2 = 3
+print (num1 * num2)
+name = "Cami"
+Address = "12442 Carmel Point"
+City = "San Diego"
+Zip = "92130"
+print (name + Address + City + Zip)
+length = 4
+width = 3
+height = 2
+print (2*(length * width + height * width + height * length))
